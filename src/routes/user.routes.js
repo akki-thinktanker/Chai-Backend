67eee33fa6348f56501dabed9f73266fd308c312
akki-thinktanker <158248599+akki-thinktanker@users.noreply.github.com>
2024-06-19 12:39:23 +0530

    add new routes for user controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   logoutUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -18,10 +25,21 @@ userRouter.route("/register").post(
   registerUser
 );
 
-userRouter.post("/login",loginUser);
+userRouter.post("/login", loginUser);
 
 // Secured routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
-userRouter.route('/refreshtoken').post(refreshAccessToken)
+userRouter.route("/refreshtoken").post(refreshAccessToken);
+userRouter.route("/changepassword").post(changeCurrentPassword);
+userRouter.route("/currentuser").get(verifyJWT, getCurrentUser);
+userRouter.route("/updateaccount").patch(verifyJWT, updateAccountDetails);
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("/coverimage")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+userRouter.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+userRouter.route("/history").get(verifyJWT, getWatchHistory);
 
 export default userRouter;
